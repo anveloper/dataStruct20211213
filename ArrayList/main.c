c8e77cf8dcde3a310f8ac8da9b5b8d4ac28f538e
zi0nman <hitedin@gmail.com>
2021-12-13 12:01:45 +0900

    자료 구조 ArrayList 구현

---
+++ ArrayList/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct _array(){
+	
+	plist 
+} List;
+
+void ListInit(List* plist){
+	
+}
+
+void LInsert(List* plist, LData data){
+	
+	
+}
+
+int main(void) {
+	
+	
+	
+	return 0;
+}
+
+
+int LFirst (List* plist, LData * pdata){
+	
+}
+
+int LNext (List* plist, LData * pdata){
+	
+}
+
+LData LRemove(){
+	
+}
