14a02a76fc582dbbb5cf5b438b8bc9f938fe9c2d
zi0nman <hitedin@gmail.com>
2021-12-14 12:06:27 +0900

    삭제, 크기 구하는 Array 함수 추가

--- ArrayList/main.c
+++ ArrayList/main.c
@@ -8,12 +8,57 @@ int main(void) {
 	int data;	
 	ListInit(&list);
 	
-	LInsert(&list, 11);
-	LInsert(&list, 55);
-	LInsert(&list, 22);
-	LInsert(&list, 44);
-	LInsert(&list, 33);
+	LInsert(&list, 1);
+	LInsert(&list, 2);
+	LInsert(&list, 3);
+	LInsert(&list, 4);
+	LInsert(&list, 5);
+	LInsert(&list, 6);
+	LInsert(&list, 7);
+	LInsert(&list, 8);
+	LInsert(&list, 9);
 	
+	// 전체 데이터 조회 
+	printf("===전체 데이터 조회===\n");
+	if(LFirst(&list, &data)) {
+		printf("%d ", data);
+		while(LNext(&list, &data)) {
+			printf("%d ", data);
+	 	}
+	}
+	//데이터의 합 구하 기
+	int sum = 0 ;
+	
+	if(LFirst(&list, &data)) {
+		sum += data;
+		while(LNext(&list, &data)) {
+			sum += data;
+	 	}
+	}
+	printf("\n===전체 데이터의 합===\n");
+	printf("%d\n",sum);
+	
+	// 2의 배수와 3의 배수 삭제하기
+	
+	printf("===2, 3의 배수 삭제===\n\n");
+	if(LFirst(&list, &data)){
+		if (data % 2 == 0){
+			LRemove(&list);
+		} 
+		else if (data % 3 == 0){
+			LRemove(&list);
+		}
+		while(LNext(&list, &data)){
+			if (data % 2 == 0){
+				LRemove(&list);
+			}
+			else if (data % 3 == 0){
+				LRemove(&list);
+			}
+		}
+	} 
+	
+	printf("===전체 데이터 조회===\n");
 	if(LFirst(&list, &data)) {
 		printf("%d ", data);
 		while(LNext(&list, &data)) {
